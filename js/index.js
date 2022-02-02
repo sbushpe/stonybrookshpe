5065da300976e9e99246e2d59f90cb4bc0beac2c
Michael Campos <camposm1205@gmail.com>
2022-02-02 17:12:05 -0500

    Added E.E.

--- js/index.js
+++ js/index.js
@@ -9,3 +9,16 @@ function showNationalSHPEsite() {
 function showMembershipPage() {
   open("https://www.shpe.org/membership")
 }
+
+let geurisClickCount = 0;
+function onClickGeuris() {
+  let elemImg = document.getElementById('img_geuris')
+  geurisClickCount++
+  if (geurisClickCount >= 10 && geurisClickCount <= 15) {
+    elemImg.src = './media/eboard-members/geuris.png'
+  }
+  if (geurisClickCount >= 15) {
+    elemImg.src = './media/eboard-members/geuris-german.png'
+    geurisClickCount = 0
+  }
+}
