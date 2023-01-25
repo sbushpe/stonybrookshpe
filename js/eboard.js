83dbd16ad7b93f74609f54f6b63b5093a651f0fd
Michael Campos <camposm.dev@gmail.com>
2023-01-25 15:36:27 -0500

    Updated Eboard Members and Contact Info Implemented eboard.js to store eboard member's title, name, email, and file name of their profile picture. eboard.js is utilized index.js to render the eboard members and their contact info rather than having to iteratively update the info in HTML.

---
+++ js/eboard.js
@@ -0,0 +1,50 @@
+const Position = {
+    PRESIDENT: 'President',
+    VICE_PRESIDENT: 'Vice President',
+    TREASURER: 'Treasurer',
+    SECRETARY: 'Secretary',
+    WEB_MASTER: 'Web Master',
+    GRAPHIC_DESIGNER: 'Graphic Designer'
+}
+
+export const eboardMembers = [
+    {
+      src: 'kevin-cruz.png',
+      title: Position.PRESIDENT,
+      name: 'Kevin Cruz',
+      email: 'kevin.cruz@stonybrook.edu'
+    },
+    {
+      src: 'jender.png',
+      title: Position.VICE_PRESIDENT,
+      name: 'Jender Baldera',
+      email: 'jender.baldera@stonybrook.edu'
+    },
+    {
+      src: 'saadat-islam.png',
+      title: Position.TREASURER,
+      name: 'Saadat Islam',
+      email: 'saadat.islam@stonybrook.edu'
+    },
+    {
+      src: 'adriana.png',
+      title: Position.SECRETARY,
+      name: 'Adriana Salomon',
+      email: 'adriana.salomoncampodonico@stonybrook.edu'
+    },
+    {
+      src: 'michael-c.png',
+      title: Position.WEB_MASTER,
+      name: 'Michael Campos'
+    },
+    {
+      src: 'josias.png',
+      title: Position.GRAPHIC_DESIGNER,
+      name: 'Josias'
+    },
+    {
+      src: 'steven-luc-huynh.png',
+      title: Position.GRAPHIC_DESIGNER,
+      name: 'Steven Luc Huynh'
+    }
+  ]
--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
+import { eboardMembers } from "./eboard.js";
+
 function showChapterBylaws() {
-    open("chapter-bylaws.html")
+  open("chapter-bylaws.html")
 }
 
 function showNationalSHPEsite() {
@@ -10,13 +12,82 @@ function showMembershipPage() {
   open("https://www.shpe.org/membership")
 }
 
-let showOriginalGeuris = false;
-function onClickGeuris() {
-  let elemImg = document.getElementById('img_geuris')
-  showOriginalGeuris = !showOriginalGeuris;
-  if (showOriginalGeuris) {
-    elemImg.src = './media/eboard-members/geuris.png'
-  } else {
-    elemImg.src = './media/eboard-members/geuris-german.png'
+class App {
+  constructor() {
+    this.showingGeuris = false
   }
+
+  start = () => {
+    this.renderEboardMembers()
+    this.renderContactInfo()
+  }
+
+  onClickTreasurer = () => {
+    let elemImg = document.getElementById('img_geuris')
+    this.showingGeuris = !this.showingGeuris
+    if (this.showingGeuris) {
+      elemImg.src = './media/eboard-members/geuris.png'
+    } else {
+      elemImg.src = './media/eboard-members/geuris-german.png'
+    }
+  }
+
+  renderEboardMembers = () => {
+    let root = document.getElementById('eboard-members')
+    if (!root) return
+    eboardMembers.forEach(m => {
+      let elem = this.initEboardMemberElem(m.src, m.title, m.name)
+      root.appendChild(elem)
+    })
+  }
+
+  renderContactInfo = () => {
+    let root = document.getElementById('contact-info-section')
+    if (!root) return
+    eboardMembers.forEach(m => {
+      if (m.email) {
+        let elem = this.initContactElem(m.title, m.name, m.email)
+        root.appendChild(elem)
+      }
+    })
+  }
+
+  initEboardMemberElem = (src, title, name) => {
+    const IMG_SIZE = 140
+    let imgElem = document.createElement('img')
+    if (title === 'Treasurer') {
+      imgElem.id = 'img_geuris'
+      imgElem.onclick = this.onClickTreasurer
+    }
+    imgElem.src = './media/eboard-members/' + src
+    imgElem.width = IMG_SIZE
+    imgElem.height = IMG_SIZE
+    let eboardPosElem = document.createElement('h2')
+    eboardPosElem.innerHTML = title
+    let nameElem = document.createElement('h3')
+    nameElem.innerHTML = name
+    let root = document.createElement('div')
+    root.classList.add('col-lg-4')
+    root.appendChild(imgElem)
+    root.appendChild(eboardPosElem)
+    root.appendChild(nameElem)
+    return root
+  }
+
+  initContactElem = (title, name, email) => {
+    let nameElem = document.createElement('h6')
+    nameElem.innerHTML = `${title} ${name}`
+    let emailElem = document.createElement('a')
+    emailElem.href = `mailto:${email}`
+    emailElem.innerHTML = email
+    let root = document.createElement('p')
+    root.appendChild(nameElem)
+    root.append(emailElem)
+    return root
+  }
+}
+
+window.onload = () => {
+  let app = new App()
+  app.start()
 }
