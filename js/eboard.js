187da4bedd8782575b7521408f32e4b1b09034cf
Adriana S <55250158+adriana-ss@users.noreply.github.com>
2023-09-06 14:30:35 -0400

    Update eboard.js
    Changed President name

--- js/eboard.js
+++ js/eboard.js
@@ -1,50 +1,50 @@
 const Position = {
-    PRESIDENT: 'President',
-    VICE_PRESIDENT: 'Vice President',
-    TREASURER: 'Treasurer',
-    SECRETARY: 'Secretary',
-    WEB_MASTER: 'Web Master',
-    GRAPHIC_DESIGNER: 'Graphic Designer'
-}
+	PRESIDENT: "President",
+	VICE_PRESIDENT: "Vice President",
+	TREASURER: "Treasurer",
+	SECRETARY: "Secretary",
+	WEB_MASTER: "Web Master",
+	GRAPHIC_DESIGNER: "Graphic Designer",
+};
 
 export const eboardMembers = [
-    {
-      src: 'kevin-cruz.png',
-      title: Position.PRESIDENT,
-      name: 'Kevin Cruz',
-      email: 'kevin.cruz@stonybrook.edu'
-    },
-    {
-      src: 'jender.png',
-      title: Position.VICE_PRESIDENT,
-      name: 'Jender Baldera',
-      email: 'jender.baldera@stonybrook.edu'
-    },
-    {
-      src: 'saadat-islam.png',
-      title: Position.TREASURER,
-      name: 'Saadat Islam',
-      email: 'saadat.islam@stonybrook.edu'
-    },
-    {
-      src: 'adriana.png',
-      title: Position.SECRETARY,
-      name: 'Adriana Salomon',
-      email: 'adriana.salomoncampodonico@stonybrook.edu'
-    },
-    {
-      src: 'michael-c.png',
-      title: Position.WEB_MASTER,
-      name: 'Michael Campos'
-    },
-    {
-      src: 'josias.png',
-      title: Position.GRAPHIC_DESIGNER,
-      name: 'Josias'
-    },
-    {
-      src: 'steven-luc-huynh.png',
-      title: Position.GRAPHIC_DESIGNER,
-      name: 'Steven Luc Huynh'
-    }
-  ]
+	{
+		src: "kevin-cruz.png",
+		title: Position.PRESIDENT,
+		name: "Josias Gonzalez",
+		email: "kevin.cruz@stonybrook.edu",
+	},
+	{
+		src: "jender.png",
+		title: Position.VICE_PRESIDENT,
+		name: "Jender Baldera",
+		email: "jender.baldera@stonybrook.edu",
+	},
+	{
+		src: "saadat-islam.png",
+		title: Position.TREASURER,
+		name: "Saadat Islam",
+		email: "saadat.islam@stonybrook.edu",
+	},
+	{
+		src: "adriana.png",
+		title: Position.SECRETARY,
+		name: "Adriana Salomon",
+		email: "adriana.salomoncampodonico@stonybrook.edu",
+	},
+	{
+		src: "michael-c.png",
+		title: Position.WEB_MASTER,
+		name: "Michael Campos",
+	},
+	{
+		src: "josias.png",
+		title: Position.GRAPHIC_DESIGNER,
+		name: "Josias",
+	},
+	{
+		src: "steven-luc-huynh.png",
+		title: Position.GRAPHIC_DESIGNER,
+		name: "Steven Luc Huynh",
+	},
+];
