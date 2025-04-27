7f801f57f0d020abca96f557fa7429a5f752d287
cdr <adarshcp2077@gmail.com>
2025-04-26 22:29:24 -0400

    updated eboard member photos and moved old eboard pictures to 2023-24-eboard/ directory

--- js/eboard.js
+++ js/eboard.js
@@ -1,60 +1,60 @@
 const Position = {
 	PRESIDENT: "President",
-	VICE_PRESIDENT: "Vice President",
+	// VICE_PRESIDENT: "Vice President",
 	TREASURER: "Treasurer",
 	SECRETARY: "Secretary",
 	WEB_MASTER: "Web Master",
-	GRAPHIC_DESIGNER: "Graphic Designer",
-	PR_CHAIR: "PR Chair",
-	EVENT_COORDINATOR: "Event Coordinator",
+	// GRAPHIC_DESIGNER: "Graphic Designer",
+	// PR_CHAIR: "PR Chair",
+	OUTREACH_CHAIR: "Outreach Chair"
 };
 
 export const eboardMembers = [
 	{
-		src: "josiasbeingprofessional.png",
+		src: "pres-avilla.jpg",
 		title: Position.PRESIDENT,
-		name: "Josias Gonzalez",
-		email: "josias.gonzalez@stonybrook.edu",
+		name: "Arelis Villa-Perea",
+		email: "arelis.villa-perea@stonybrook.edu",
 	},
+	// {
+	// 	src: "2023-24-eboard/Kevin.jpg",
+	// 	title: Position.VICE_PRESIDENT,
+	// 	name: "Kevin Cruz",
+	// 	email: "kevin.cruz@stonybrook.edu",
+	// },
 	{
-		src: "Kevin.jpg",
-		title: Position.VICE_PRESIDENT,
-		name: "Kevin Cruz",
-		email: "kevin.cruz@stonybrook.edu",
-	},
-	{
-		src: "oscar.jpeg",
+		src: "treasurer-aLam.png",
 		title: Position.TREASURER,
-		name: "Oscar Berrios",
-		email: "oscar.berrios@stonybrook.edu",
+		name: "Mario Lam",
+		email: "mario.lam@stonybrook.edu",
 	},
 	{
-		src: "jeremy.jpg",
+		src: "secy-arod.jpg",
 		title: Position.SECRETARY,
-		name: "Jeremy Galindo",
-		email: "Jeremy.Galindo@stonybrook.edu",
+		name: "Adrian Rodriguez",
+		email: "adrian.m.rodriguez@stonybrook.edu",
 	},
 	{
-		src: "alam.jpg",
+		src: "2023-24-eboard/alam.jpg",
 		title: Position.WEB_MASTER,
 		name: "Alam Zishan",
 	},
 	
-	{
-		src: "camilo.JPG",
-		title: Position.GRAPHIC_DESIGNER,
-		name: "Camilo Arias",
-	},
+	// {
+	// 	src: "2023-24-eboard/camilo.JPG",
+	// 	title: Position.GRAPHIC_DESIGNER,
+	// 	name: "Camilo Arias",
+	// },
 	
+	// {
+	// 	src: "2023-24-eboard/Nicole.Mena.jpg",
+	// 	title: Position.PR_CHAIR,
+	// 	name: "Nicole Mena",
+	// },
 	{
-		src: "Nicole.Mena.jpg",
-		title: Position.PR_CHAIR,
-		name: "Nicole Mena",
-	},
-	{
-		src:"Eduardo.jpg",
-		title: Position.EVENT_COORDINATOR,
-		name: "Eduardo Lozano"
+		src:"oc-jaqueron.jpg",
+		title: Position.OUTREACH_CHAIR,
+		name: "Jabe Aqueron"
 	}
 	
 ];
